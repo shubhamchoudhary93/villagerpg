c6fb1e298f3892f42f2b584d6402f67dc89d284f
shubham <shubhamchoudhary@outlook.com>
2022-05-20 08:00:10 +0530

    kotlin and packages upgraded

--- app/src/main/java/com/shubham/villagerpg/ExplorePageFragment.kt
+++ app/src/main/java/com/shubham/villagerpg/ExplorePageFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -81,7 +82,7 @@ class ExplorePageFragment : Fragment() {
                 names
             )
         binding.foodDropdown.adapter = adapter
-
+        mainHandler = Handler(Looper.getMainLooper())
         setScreenData()
         setListeners()
         val title = "Explore"
